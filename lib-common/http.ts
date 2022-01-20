b0a907507dcb48f341812f2e7000c1987beb32cf
yexinhao <ye.xinhao@oushu.io>
2022-01-20 17:17:26 +0800

    feat:401支持

--- lib-common/http.ts
+++ lib-common/http.ts
@@ -1,6 +1,8 @@
+import { useRouter } from "vue-router";
 import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios'
 import message from 'ant-design-vue/lib/message'
 import 'ant-design-vue/lib/message/style/css'
+
 
 // const TIMEOUT = 10000
 export const SUCCESS_STATUS_CODE = 200
@@ -29,13 +31,24 @@ http.interceptors.response.use((res: AxiosResponse<Response<any>>) => {
     message.error('接口格式错误:没有meta')
     return Promise.reject(new Error('接口格式错误:没有meta'))
   }
-
   return {
     ...(res.data || {}),
     meta: res.data.meta || {},
   }
 }, (err) => {
-  message.error(err.message)
+  const errObj: any = JSON.parse(JSON.stringify(err))
+  if (errObj.status === 401) {
+    if (sessionStorage.getItem('is401') === '1') {
+      return err
+    } else {
+      sessionStorage.setItem('is401', '1')
+      message.error(errObj.message)
+      setTimeout(() => {
+        useRouter().push('/login')
+      })
+      return Promise.reject(err)
+    }
+  }
   return Promise.reject(err)
 })
 
